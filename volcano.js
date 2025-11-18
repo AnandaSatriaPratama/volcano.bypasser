96492fa63626816bc5b4c9ba25a08d4cc49f92c3
Ananda Satria Pratama <159537467+AnandaSatriaPratama@users.noreply.github.com>
2025-11-18 14:12:02 +0700

    Create volcano.js

---
+++ volcano.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Nan Bypasser
+// @namespace    http://tampermonkey.net/
+// @version      v1.0.0
+// @description  bypasss workink and volcano key system
+// @author       Nan
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @grant        GM_setClipboard
+// @icon         https://shorturl.at/eNOJ9
+// @connect      *
+// @downloadURL  https://github.com/AnandaSatriaPratama/volcano.bypasser/raw/main/volcano.js
+// @updateURL    https://github.com/AnandaSatriaPratama/volcano.bypasser/raw/main/volcano.js
+// @require https://github.com/AnandaSatriaPratama/volcano.bypasser/raw/refs/heads/main/bypass.js
+// ==/UserScript==
